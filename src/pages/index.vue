c5d7fe456b84775b6d00bd74fac8f15e05f7bca9
bowerchen <bowerfullstack@gmail>
2020-11-13 18:52:01 +0800

    main

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,55 @@
     <div class="index"> 
         <div class="container">
             <div class="swiper-box">
+                <div class="nav-menu">
+                    <ul class="menu-wrap">
+                        <li class="menu-item">
+                            <a href="javascript:;">手机  电话卡</a>
+                            <div class="children">
+                                <ul v-for="(item, index) in menuList" :key="index">
+                                    <li v-for="(sub,index) in item" :key="index">
+                                        <a :href="sub ? '/#/product/' + sub.id : ''">
+                                            <img :src="sub ? sub.img : '/imgs/item-box-1.png'" alt="">
+                                            {{sub ? sub.name : 小米9}}
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">电视 盒子</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">笔记本 显示器</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">家电 插线板</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">智能 路由器</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">电源 配件</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">健康 儿童</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">耳机 音响</a>
+                            <div class="children"></div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">生活 箱包</a>
+                            <div class="children"></div>
+                        </li>
+                    </ul>
+                </div>
                 <swiper ref="indexSwiper" :options="swiperOptions">
                     <swiper-slide v-for="(item, index) in slideList" v-bind:key="index">
                         <a v-bind:href="'/#/product/'+ item.id "><img v-bind:src="item.img"></a>
@@ -76,6 +125,163 @@
                         img:'/imgs/slider/slide-5.jpg'
                     },
                     
+                ],
+                menuList: [
+                    [
+                        {
+                            id: 30,
+                            img: '/imgs/item-box-1.png',
+                            name: '小米CC9',
+                        },
+                        {
+                            id: 31,
+                            img: '/imgs/item-box-2.png',
+                            name: '小米8青春版',
+                        },
+                        {
+                            id: 32,
+                            img: '/imgs/item-box-3.jpg',
+                            name: 'redMi K20 Pro',
+                        },
+                        {
+                            id: 33,
+                            img: '/imgs/item-box-4.jpg',
+                            name: '移动4G专区',
+                        },
+                    ],
+                    [
+                        {
+                            id: 30,
+                            img: '/imgs/item-box-1.png',
+                            name: '小米CC9',
+                        },
+                        {
+                            id: 31,
+                            img: '/imgs/item-box-2.png',
+                            name: '小米8青春版',
+                        },
+                        {
+                            id: 32,
+                            img: '/imgs/item-box-3.jpg',
+                            name: 'redMi K20 Pro',
+                        },
+                        {
+                            id: 33,
+                            img: '/imgs/item-box-4.jpg',
+                            name: '移动4G专区',
+                        },
+                    ],
+                    [
+                        {
+                            id: 30,
+                            img: '/imgs/item-box-1.png',
+                            name: '小米CC9',
+                        },
+                        {
+                            id: 31,
+                            img: '/imgs/item-box-2.png',
+                            name: '小米8青春版',
+                        },
+                        {
+                            id: 32,
+                            img: '/imgs/item-box-3.jpg',
+                            name: 'redMi K20 Pro',
+                        },
+                        {
+                            id: 33,
+                            img: '/imgs/item-box-4.jpg',
+                            name: '移动4G专区',
+                        },
+                    ],
+                    [
+                        {
+                            id: 30,
+                            img: '/imgs/item-box-1.png',
+                            name: '小米CC9',
+                        },
+                        {
+                            id: 31,
+                            img: '/imgs/item-box-2.png',
+                            name: '小米8青春版',
+                        },
+                        {
+                            id: 32,
+                            img: '/imgs/item-box-3.jpg',
+                            name: 'redMi K20 Pro',
+                        },
+                        {
+                            id: 33,
+                            img: '/imgs/item-box-4.jpg',
+                            name: '移动4G专区',
+                        },
+                    ],
+                    [
+                        {
+                            id: 30,
+                            img: '/imgs/item-box-1.png',
+                            name: '小米CC9',
+                        },
+                        {
+                            id: 31,
+                            img: '/imgs/item-box-2.png',
+                            name: '小米8青春版',
+                        },
+                        {
+                            id: 32,
+                            img: '/imgs/item-box-3.jpg',
+                            name: 'redMi K20 Pro',
+                        },
+                        {
+                            id: 33,
+                            img: '/imgs/item-box-4.jpg',
+                            name: '移动4G专区',
+                        },
+                    ],
+                    [
+                        {
+                            id: 30,
+                            img: '/imgs/item-box-1.png',
+                            name: '小米CC9',
+                        },
+                        {
+                            id: 31,
+                            img: '/imgs/item-box-2.png',
+                            name: '小米8青春版',
+                        },
+                        {
+                            id: 32,
+                            img: '/imgs/item-box-3.jpg',
+                            name: 'redMi K20 Pro',
+                        },
+                        {
+                            id: 33,
+                            img: '/imgs/item-box-4.jpg',
+                            name: '移动4G专区',
+                        },
+                    ],
+                    [
+                        {
+                            id: 30,
+                            img: '/imgs/item-box-1.png',
+                            name: '小米CC9',
+                        },
+                        {
+                            id: 31,
+                            img: '/imgs/item-box-2.png',
+                            name: '小米8青春版',
+                        },
+                        {
+                            id: 32,
+                            img: '/imgs/item-box-3.jpg',
+                            name: 'redMi K20 Pro',
+                        },
+                        {
+                            id: 33,
+                            img: '/imgs/item-box-4.jpg',
+                            name: '移动4G专区',
+                        },
+                    ],
+                   
                 ]
             }
         },
@@ -91,14 +297,91 @@
 </script>
 
 <style scoped lang="scss">
+@import './../assets/scss/mixin.scss';
+@import './../assets/scss/config.scss';
 .index {
     .swiper-box {
+        .nav-menu {
+            position: absolute;
+            width: 264px;
+            height: 451px;
+            z-index: 9;
+            box-sizing: border-box;
+            background-color: rgba(#55585a, .7);
+            .menu-wrap {
+                .menu-item {
+                    height: 50px;
+                    line-height: 50px;
+                    a {
+                        display: block;
+                        position: relative;
+                        font-size: 16px;
+                        color: #fff;
+                        padding-left: 30px;
+                        &:after {
+                            position: absolute;
+                            right: 30px;
+                            top: 17.5px;
+                            content: ' ';
+                            @include bgImg(10px, 15px, '/imgs/icon-arrow.png');
+                        }
+                    }
+                    &:hover {
+                        background-color: $colorA;
+                        .children {
+                            display: block;
+                        }
+                        
+                    }
+                    .children {
+                        display: none;
+                        position: absolute;
+                        top: 0;
+                        left: 264px;
+                        border: 1px solid $colorH;
+                        width: 962px;
+                        height: 451px;
+                        background: $colorG;
+                        overflow: hidden;
+                        // z-index: 9;
+                        ul {
+                            display: flex;
+                            position: relative;
+                            justify-content: space-between;
+                            height: 75px;
+                            z-index: 1;
+                            li {
+                                flex: 1;
+                                padding-left: 23px;
+                                width: 241px;
+                                height: 75px;
+                                line-height: 75px;
+                            }
+                            a {
+                                color: $colorB;
+                                font-size: 14px;
+                            }
+                            img {
+                                width: 42px;
+                                height: 35px;
+                                vertical-align: middle;
+                                margin-right: 15px;
+                            }
+                        }
+                    }
+                }
+            }
+        }
         .swiper-container {
             height: 451px;
+            .swiper-button-prev {
+                left: 274px;
+            }
             img {
                 width: 100%;
                 height: 100%;
             }
+            
         }
     }
 }
